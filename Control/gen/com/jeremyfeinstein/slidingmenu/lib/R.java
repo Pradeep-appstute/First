0e8889888cc91340d7a718c081b5ec8ae4fbcb3d
shivkumarappstute <AppstuteMac5@Appstutes-MacBook-Pro.local>
2014-02-11 15:48:35 +0530

    Code cleanup completed

--- Control/gen/com/jeremyfeinstein/slidingmenu/lib/R.java
+++ Control/gen/com/jeremyfeinstein/slidingmenu/lib/R.java
@@ -27,22 +27,22 @@ public final class R {
 		public static final int ic_launcher = 0x7f020072;
 	}
 	public static final class id {
-		public static final int fullscreen = 0x7f060013;
-		public static final int left = 0x7f060010;
-		public static final int margin = 0x7f060012;
-		public static final int right = 0x7f060011;
-		public static final int selected_view = 0x7f060014;
-		public static final int slidingmenumain = 0x7f060041;
+		public static final int fullscreen = 0x7f070013;
+		public static final int left = 0x7f070010;
+		public static final int margin = 0x7f070012;
+		public static final int right = 0x7f070011;
+		public static final int selected_view = 0x7f070014;
+		public static final int slidingmenumain = 0x7f070040;
 	}
 	public static final class layout {
 		public static final int slidingmenumain = 0x7f03001d;
 	}
 	public static final class string {
-		public static final int app_name = 0x7f040000;
+		public static final int app_name = 0x7f050000;
 	}
 	public static final class style {
-		public static final int AppBaseTheme = 0x7f050000;
-		public static final int AppTheme = 0x7f050001;
+		public static final int AppBaseTheme = 0x7f060000;
+		public static final int AppTheme = 0x7f060001;
 	}
 	public static final class styleable {
 		public static final int[] SlidingMenu = { 0x7f010062, 0x7f010063, 0x7f010064, 0x7f010065, 0x7f010066, 0x7f010067, 0x7f010068, 0x7f010069, 0x7f01006a, 0x7f01006b, 0x7f01006c, 0x7f01006d, 0x7f01006e, 0x7f01006f };
