d637b6b395ae7cc9555d5838f291367fc894ffb0
shivkumarappstute <AppstuteMac5@Appstutes-MacBook-Pro.local>
2014-03-31 16:36:35 +0530

    Tab bar images edited

---
+++ TestProject/Control/src/com/masergy/iscticket/utility/CommonResources.java
@@ -0,0 +1,12 @@
+package com.masergy.iscticket.utility;
+
+public class CommonResources {
+
+	//Development server links
+	public static String prefixLink ="https://webservice-dev.masergy.com/webservices_mobile/rest/v1/";
+	public static String prefixLinkContactUs ="https://webservice-dev.masergy.com/webservices_mobile/";
+	
+	//Production server links
+//	public static String prefixLink ="https://webservice.masergy.com/webservices_mobile/rest/v1/";
+//	public static String prefixLinkContactUs ="https://webservice.masergy.com/webservices_mobile/";
+}
