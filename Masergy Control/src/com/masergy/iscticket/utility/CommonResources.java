9148aa632d618f298914d18af0e6b4dcf6df213f
shivkumarappstute <AppstuteMac5@Appstutes-MacBook-Pro.local>
2014-04-08 17:36:48 +0530

    Resolved Bug 540 - Doppler IM: Some records are not expanding

--- Masergy Control/src/com/masergy/iscticket/utility/CommonResources.java
+++ Masergy Control/src/com/masergy/iscticket/utility/CommonResources.java
@@ -1,5 +1,7 @@
 package com.masergy.iscticket.utility;
 
+import android.util.Log;
+
 public class CommonResources {
 
 	//Development server links
@@ -9,4 +11,9 @@ public class CommonResources {
 	//Production server links
 //	public static String prefixLink ="https://webservice.masergy.com/webservices_mobile/rest/v1/";
 //	public static String prefixLinkContactUs ="https://webservice.masergy.com/webservices_mobile/";
+	
+//	public void displayLog(String tag, String message)
+//	{
+//		Log.d(tag, message);
+//	}
 }
